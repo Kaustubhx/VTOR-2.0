86772690c1c9c326659b250423490958f18fa8a2
Kastubhx <prime.ganekar@gmail.com>
2022-10-03 00:06:17 +0530

    New: Products Page

---
+++ src/app/pages/myybaproduct/myybaproduct.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'myybaproduct',
+  templateUrl: './myybaproduct.component.html',
+  styleUrls: ['./myybaproduct.component.css']
+})
+export class MyybaproductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
