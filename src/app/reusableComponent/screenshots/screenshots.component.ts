9344bd57d3b53e40ce2b2a4c77d70c6b920e41a4
Kastubhx <prime.ganekar@gmail.com>
2022-10-05 19:51:58 +0530

    Fix & Add: Styling on cetrain pages & per app screenshots

--- src/app/reusableComponent/screenshots/screenshots.component.ts
+++ src/app/reusableComponent/screenshots/screenshots.component.ts
@@ -40,17 +40,22 @@ export class ScreenshotsComponent implements OnInit {
 
   appScreenshots = [
     {
-      appImage: 'https://play-lh.googleusercontent.com/a-KhO5en9zsc1NI9P9RTI83x7anKSgPTfY5kGulKAqRRPMBwdvojkalC4gCtWJxG-gAK=w2560-h1440-rw'
-    },
-    {
-      appImage: 'https://play-lh.googleusercontent.com/UP9Nwu2LEHAJHGzn57yMCTtp1oUW6jqfDtp03W0mZFFsoq2RoD7HTzIW8rn5Bkoy0w=w2560-h1440-rw'
-    },
-    {
-      appImage: 'https://play-lh.googleusercontent.com/3V9QBR_nrBHl5PFlblmnH6ghqb33oX9pa3_PAhKOklYfu-f0VWdeVkYQcHnueMMcTw=w2560-h1440-rw'
-    },
-    {
-      appImage: 'https://play-lh.googleusercontent.com/XQVcn1a6quDH00u-SbWI_VEl3Qd3QNi2-uVAf0R4tfeVjQSOXmDzrblt_xv0lRTj-Lc2=w2560-h1440-rw'
-    },
+      myybaa: [
+        {
+          appImage: 'https://play-lh.googleusercontent.com/a-KhO5en9zsc1NI9P9RTI83x7anKSgPTfY5kGulKAqRRPMBwdvojkalC4gCtWJxG-gAK=w2560-h1440-rw'
+        },
+        {
+          appImage: 'https://play-lh.googleusercontent.com/UP9Nwu2LEHAJHGzn57yMCTtp1oUW6jqfDtp03W0mZFFsoq2RoD7HTzIW8rn5Bkoy0w=w2560-h1440-rw'
+        },
+        {
+          appImage: 'https://play-lh.googleusercontent.com/3V9QBR_nrBHl5PFlblmnH6ghqb33oX9pa3_PAhKOklYfu-f0VWdeVkYQcHnueMMcTw=w2560-h1440-rw'
+        },
+        {
+          appImage: 'https://play-lh.googleusercontent.com/XQVcn1a6quDH00u-SbWI_VEl3Qd3QNi2-uVAf0R4tfeVjQSOXmDzrblt_xv0lRTj-Lc2=w2560-h1440-rw'
+        },
+      ]
+    }
+
   ]
 
 }
