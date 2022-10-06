c88bc23a4068acb4668557cb0d380746bd3b2058
Kastubhx <prime.ganekar@gmail.com>
2022-10-06 23:45:10 +0530

    New: Made team section dynamic

--- src/app/pages/aboutus/aboutus.component.ts
+++ src/app/pages/aboutus/aboutus.component.ts
@@ -7,35 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutusComponent implements OnInit {
 
-  labsStoryData = [
+  vtorTeamData = [
     {
-      bgColor: '#0083ff',
-      yearImg: 'https://html.crumina.net/html-utouch/svg/flag.svg',
-      year: '2019',
-      yearTitle: 'Claritas est processus',
-      yearInfo: 'Wisi enim ad minim veniam, quis nostrud exerci tation qui nunc nobis videntur parum clari.'
+      memberAvatar: 'https://html.crumina.net/html-utouch/img/teammember3.png',
+      memberRole: 'Founder',
+      memberName: 'Abhijit Chaudhry',
+      memberIntro: 'Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.',
     },
     {
-      bgColor: '#EF6517',
-      yearImg: 'https://html.crumina.net/html-utouch/svg/settings.svg',
-      year: '2020',
-      yearTitle: 'Typi non claritatem',
-      yearInfo: 'Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit lectores legere me lius quod ii legunt.',
+      memberAvatar: 'https://html.crumina.net/html-utouch/img/teammember3.png',
+      memberRole: 'Founder',
+      memberName: 'Moxesh Mehta',
+      memberIntro: 'Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.',
     },
     {
-      bgColor: '#ff3133',
-      yearImg: 'https://html.crumina.net/html-utouch/svg/team.svg',
-      year: '2021',
-      yearTitle: 'Eodem modo typi',
-      yearInfo: 'Investigationes demonstraverunt lectores legere me lius quod.',
-    },
-    {
-      bgColor: '#01a23c',
-      yearImg: 'https://html.crumina.net/html-utouch/svg/rocket-launch.svg',
-      year: '2022',
-      yearTitle: 'Duis autem vel eum',
-      yearInfo: 'Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip.',
-    },
+      memberAvatar: 'https://html.crumina.net/html-utouch/img/teammember2.png',
+      memberRole: 'Founder',
+      memberName: 'Britney Simpson',
+      memberIntro: 'Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.',
+    }
   ]
 
   constructor() { }
