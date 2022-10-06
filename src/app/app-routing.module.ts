94cb22915f342217b81ca6ee6c68bb4614f4ab80
Kastubhx <prime.ganekar@gmail.com>
2022-10-06 23:43:10 +0530

    Add: Fitapps page to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { FitappspageComponent } from './pages/fitappspage/fitappspage.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { MyybaproductComponent } from './pages/myybaproduct/myybaproduct.component';
 
 const routes: Routes = [
+  { path: 'fitappcasedetails', component: FitappspageComponent },
   { path: 'myybacasedetails', component: MyybaproductComponent },
   { path: 'about', component: AboutusComponent },
   { path: '', component: HomepageComponent },
