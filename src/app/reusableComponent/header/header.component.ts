44848770d0ee9d35aab42def1de179bc9562ec27
Kastubhx <prime.ganekar@gmail.com>
2022-10-11 23:44:58 +0530

    New: Scroll to caseslider

--- src/app/reusableComponent/header/header.component.ts
+++ src/app/reusableComponent/header/header.component.ts
@@ -15,6 +15,16 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  // scrollToProducts() {
+  //   (function smoothscroll() {
+  //     var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  //     if (currentScroll > 0) {
+  //       window.requestAnimationFrame(smoothscroll);
+  //       window.scrollTo(0, currentScroll - (currentScroll / 0));
+  //     }
+  //   })();
+  // }
+
   ngOnInit(): void {
   }
 
