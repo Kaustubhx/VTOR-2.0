7eece11c8bd96b51ad63ce6993c2ccedf22c8dcb
Kastubhx <prime.ganekar@gmail.com>
2022-10-12 14:31:46 +0530

    Update: Added screenshoots back into single page

--- src/app/reusableComponent/vtorapp/vtorapp.component.ts
+++ src/app/reusableComponent/vtorapp/vtorapp.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 import { vtorApp } from 'src/app/interfaces/vtorAppDetails';
 
 @Component({
@@ -7,6 +8,31 @@ import { vtorApp } from 'src/app/interfaces/vtorAppDetails';
   styleUrls: ['./vtorapp.component.css']
 })
 export class VtorappComponent implements OnInit {
+
+  galleryOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 300,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: false
+  }
 
   @Input() appDetail: vtorApp = {} as vtorApp
 
